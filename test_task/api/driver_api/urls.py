bcedceb5cae23c0b55706872f65c480b75996efc
dima-trotsiuk <dima.trotisuk322@gmail.com>
2022-08-15 14:50:46 +0300

    Додав аргументи "name" для ідентифікації в тестах

--- test_task/api/driver_api/urls.py
+++ test_task/api/driver_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import DriverAPIListCreate, DriverAPIRetrieveUpdateDestroy
 
 driver_urlpatterns = [
-    path('driver/', DriverAPIListCreate.as_view()),
-    path('driver/<int:pk>/', DriverAPIRetrieveUpdateDestroy.as_view()),
+    path('driver/', DriverAPIListCreate.as_view(), name='list_drivers'),
+    path('driver/<int:pk>/', DriverAPIRetrieveUpdateDestroy.as_view(), name='detail_driver'),
 ]
